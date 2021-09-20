dd7aa36234b98b7db2026ca0180132ab8e29f265
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-09-20 19:33:51 +0300

    Update global styles

--- src/app/styles/GlobalStyles.jsx
+++ src/app/styles/GlobalStyles.jsx
@@ -2,6 +2,30 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
 
+ html {
+    font-size: 62.5%;
+    font-family: 'Roboto', sans-serif;
+    background-color: #f1e7de7a;
+ }
+
+ body {
+    font-size: 16px;
+    line-height: 1.6;
+    overflow-x: hidden;
+ }
+ 
+ html, body, #root {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+ }
+
+h1, h3 {
+    font-family: 'Exo', sans-serif;
+}
 `
 
 export default GlobalStyles
