0441e4ca20411fa9856f9b196aefc6df83149604
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-09-20 19:35:19 +0300

    Make layout responsive and update styles

--- src/app/layout/Layout.jsx
+++ src/app/layout/Layout.jsx
@@ -2,12 +2,19 @@ import React from "react"
 import styled from "styled-components"
 
 const Layout = ({ children }) => {
-  return <Container>{children}</Container>
+  return <LayoutContainer>{children}</LayoutContainer>
 }
 
-const Container = styled.div`
+const LayoutContainer = styled.div`
   display: flex;
   justify-content: center;
+  margin: 5rem auto;
+  max-width: 90vw;
+  min-height: 90vh;
+
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
 `
 
 export default Layout
