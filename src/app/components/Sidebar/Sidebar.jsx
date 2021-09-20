4e634816b3dbbbe979728fde9b6727449e9911a5
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-09-20 19:36:50 +0300

    Update container of the Sidebar component's styles

--- src/app/components/Sidebar/Sidebar.jsx
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,24 @@
 import React from "react"
+import styled from 'styled-components'
 import Search from "./Search"
 import Categories from './Categories'
 
 const Sidebar = () => {
   return (
-    <div>
+    <SidebarContainer>
       <Search />
       <Categories />
-    </div>
+    </SidebarContainer>
   )
 }
+
+const SidebarContainer = styled.div`
+  width: 17vw;
+  overflow:hidden;
+
+  @media (max-width: 960px) {
+    width: 100%;
+  }
+`
 
 export default Sidebar
